de144afaf51c1a9c2cf898a3e2984b3e58ae32d2
huskydoge <hbh001098hbh@sjtu.edu.cn>
2024-02-21 19:13:15 +0800

    changed table style

--- src/components/mainPage/dataRender/listiew/listView.tsx
+++ src/components/mainPage/dataRender/listiew/listView.tsx
@@ -50,93 +50,85 @@ import CloseIcon from "@mui/icons-material/Close";
 //@ts-ignore
 const ListView = ({ onClose }) => {
   return (
-    <div>
-      <Paper
-        sx={{ width: "100%", paddingLeft: 2, paddingRight: 2, paddingTop: 2 }}
+    <Paper
+      sx={{ width: "100%", paddingLeft: 2, paddingRight: 2, paddingTop: 2 }}
+    >
+      {" "}
+      {/* 确保这一层也是充满父元素的 */}
+      <Toolbar
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          p: 2,
+          width: "100%",
+        }}
       >
-        {" "}
-        {/* 确保这一层也是充满父元素的 */}
-        <Toolbar
-          sx={{
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-            p: 2,
-            width: "100%",
-          }}
-        >
-          <Typography variant="h6">Main Advisor</Typography>
-          <IconButton onClick={onClose} color="inherit">
-            <CloseIcon />
-          </IconButton>
-        </Toolbar>
-        <TableContainer component={Paper} sx={{ width: "100%" }}>
-          <Table stickyHeader>
-            <TableHead>
-              <TableRow>
-                <TableCell align="center">Advisor Name</TableCell>
-                <TableCell align="center">Position</TableCell>
-                <TableCell align="center">Affliation</TableCell>
-                <TableCell align="center">HomePage</TableCell>
-                <TableCell align="center">Email</TableCell>
-                <TableCell align="center">Twitter</TableCell>
-                <TableCell align="center">GitHub</TableCell>
-                <TableCell align="center">Collaborations</TableCell>
+        <Typography variant="h6">Main Advisor</Typography>
+        <IconButton onClick={onClose} color="inherit">
+          <CloseIcon />
+        </IconButton>
+      </Toolbar>
+      <TableContainer component={Paper} sx={{ width: "100%" }}>
+        <Table stickyHeader>
+          <TableHead>
+            <TableRow>
+              <TableCell align="center">Advisor Name</TableCell>
+              <TableCell align="center">Position</TableCell>
+              <TableCell align="center">Affliation</TableCell>
+              <TableCell align="center">HomePage</TableCell>
+              <TableCell align="center">Email</TableCell>
+              <TableCell align="center">Twitter</TableCell>
+              <TableCell align="center">GitHub</TableCell>
+              <TableCell align="center">Collaborations</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {advisors.map((advisor) => (
+              <TableRow sx={{ border: "none" }} key={advisor.advisor_id} hover>
+                <TableCell align="center">{advisor.name}</TableCell>
+                <TableCell align="center">{advisor.position}</TableCell>
+                <TableCell align="center">{advisor.affiliation}</TableCell>
+                <TableCell align="center">
+                  <Link
+                    href={advisor.homepage}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {advisor.homepage}
+                  </Link>
+                </TableCell>
+                <TableCell align="center">
+                  <Link href={`mailto:${advisor.email}`}>{advisor.email}</Link>
+                </TableCell>
+                <TableCell align="center">
+                  <Link
+                    href={advisor.twitter}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {advisor.twitter}
+                  </Link>
+                </TableCell>
+                <TableCell align="center">
+                  <Link
+                    href={advisor.github}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {advisor.github}
+                  </Link>
+                </TableCell>
+                <TableCell align="center">
+                  {/* Implementation for collaborations and connection cell */}
+                  {/* You can add a button or link here to navigate to the connection table */}
+                </TableCell>
               </TableRow>
-            </TableHead>
-            <TableBody>
-              {advisors.map((advisor) => (
-                <TableRow
-                  sx={{ border: "none" }}
-                  key={advisor.advisor_id}
-                  hover
-                >
-                  <TableCell align="center">{advisor.name}</TableCell>
-                  <TableCell align="center">{advisor.position}</TableCell>
-                  <TableCell align="center">{advisor.affiliation}</TableCell>
-                  <TableCell align="center">
-                    <Link
-                      href={advisor.homepage}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      {advisor.homepage}
-                    </Link>
-                  </TableCell>
-                  <TableCell align="center">
-                    <Link href={`mailto:${advisor.email}`}>
-                      {advisor.email}
-                    </Link>
-                  </TableCell>
-                  <TableCell align="center">
-                    <Link
-                      href={advisor.twitter}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      {advisor.twitter}
-                    </Link>
-                  </TableCell>
-                  <TableCell align="center">
-                    <Link
-                      href={advisor.github}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      {advisor.github}
-                    </Link>
-                  </TableCell>
-                  <TableCell align="center">
-                    {/* Implementation for collaborations and connection cell */}
-                    {/* You can add a button or link here to navigate to the connection table */}
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </Paper>
-    </div>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Paper>
   );
 };
 
