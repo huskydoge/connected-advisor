5d167d14171db4c5fcc729dee2f84b37b3e40dd8
huskydoge <hbh001098hbh@sjtu.edu.cn>
2024-04-29 18:09:22 +0800

    added new tag

--- src/components/const.tsx
+++ src/components/const.tsx
@@ -2,7 +2,7 @@
  * @Author: huskydoge hbh001098hbh@sjtu.edu.cn
  * @Date: 2024-04-22 20:25:48
  * @LastEditors: huskydoge hbh001098hbh@sjtu.edu.cn
- * @LastEditTime: 2024-04-25 22:14:09
+ * @LastEditTime: 2024-04-29 18:03:43
  * @FilePath: /connected-advisor/src/components/const.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -32,6 +32,7 @@ export const availableTags = [
   "3DV",
   "AI4Science",
   "Graph Learning",
+  "combinotorial optimization",
 ];
 
 export const availablePositions = [
